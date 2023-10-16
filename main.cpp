53f39edee9d615d250da02863dac1353d3de3e48
Bilal <bilal.ayachi@hetmml.nl>
2023-10-17 00:30:09 +0200

    added start screen basis

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 #include <vector>
 
 int main () {
+    std::cout << "\t\t Welcome John to Rehearse Now...\n ";
+    std::cout << "1>>>>> Create worldList \n";
+    std::cout << "2>>>>> import file \n";
+    std::cout << "3>>>>> Share wordList \n";
+    std::cout << "4>>>>> browse trough wordlist \n";
+    std::cout << "Chooce an option: \n";
     return 0;
 }
